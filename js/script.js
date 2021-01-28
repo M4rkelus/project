d59e6278a7c7adc9c6c3b0def2e633457e6a1b11
Mark <example@gmalil.com>
2021-01-28 13:52:09 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+"use sctrict";
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть уже 18", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Mark";
+
+// alert(`привет, ${user}`);
+
+console.log('arr' + " - object");
+console.log(4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+
+console.log(2*4 == 8);
+
+console.log(2 + 2 * 2 === 8);
+
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked || !isClose);
+
+
+
+
+
